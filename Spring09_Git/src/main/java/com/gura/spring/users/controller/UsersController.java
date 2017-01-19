8ad4fc97545f68dc8d978e36a75d74d4c3525e33
kimjinkuk <kimjinkuk7@naver.com>
2017-01-19 16:46:50 +0900

    회원가입 로그인 로그아웃 기능을 구현하기위한 기본 구성

---
+++ Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
@@ -0,0 +1,13 @@
+package com.gura.spring.users.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.gura.spring.users.service.UsersService;
+
+@Controller
+public class UsersController {
+
+	@Autowired
+	private UsersService usersService;
+}
