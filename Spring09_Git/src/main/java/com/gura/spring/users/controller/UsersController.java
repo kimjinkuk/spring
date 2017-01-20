ec9e9ce5b1843217c889b6ac6501d31ee1cdd6dd
kimjinkuk <kimjinkuk7@naver.com>
2017-01-20 12:21:04 +0900

    로그인 기능 구현

--- Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
+++ Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.gura.spring.users.controller;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,31 +16,91 @@ import org.springframework.web.servlet.ModelAndView;
 import com.gura.spring.users.dto.UsersDto;
 import com.gura.spring.users.service.UsersService;
 
+// component 스켄시 bean 이되고 또한 컨트롤러 역활을 할수 있도록
 @Controller
 public class UsersController {
-
+	//의존 객체 주입 되도록 
 	@Autowired
 	private UsersService usersService;
 	
-	@RequestMapping("/users/signup_form")
-	public String signup_form(){
-		return "users/signup_form";
+	// "/users/signin.do" 로그인 요청 처리
+	@RequestMapping("/users/signin")
+	public ModelAndView signin(@ModelAttribute UsersDto dto,
+			@RequestParam String uri, HttpSession session){
+		//아이디가 비밀번호가 유효한지 여부를 확인한다. 
+		boolean isValid=usersService.isValid(dto);
+		ModelAndView mView=new ModelAndView();
+		if(isValid){ //아이디 비밀번호가 맞는 정보인 경우
+			//로그인 처리를 해준다.
+			session.setAttribute("id", dto.getId());
+			mView.addObject("msg", dto.getId()+" 님 로그인 되었습니다.");
+			mView.addObject("redirectUri", uri);
+		}else{
+			//아이디 혹은 비밀번호가 틀리다는 정보를 응답한다.
+			mView.addObject("msg", "아이디 혹은 비밀번호가 틀려요");
+			String location=session.getServletContext().getContextPath()+
+					"/users/signin_form.do?uri="+uri;
+			mView.addObject("redirectUri", location);
+		}
+		//알림 페이지로 forward 이동 시킨다. 
+		mView.setViewName("users/alert");
+		return mView;
 	}
 	
-	@RequestMapping("/users/checkid")
-	@ResponseBody
-	public Map<String, Object> checkid(@RequestParam String inputId){
-		Map<String, Object> map = usersService.canUseId(inputId);
-		return map;
+	
+	// "/users/signin_form.do" 로그인 폼 요청 처리
+	@RequestMapping("/users/signin_form")
+	public String signinForm(HttpSession session){
+		//세션 초기화
+		session.invalidate();
+		//뷰페이지로 forward 이동
+		return "users/signin_form";
 	}
 	
+	// "/users/signup.do" 요청처리
 	@RequestMapping("/users/signup")
-	public ModelAndView signup(HttpServletRequest request ,@ModelAttribute UsersDto dto){
+	public ModelAndView signup(HttpServletRequest request,
+			@ModelAttribute UsersDto dto){
+		//서비스를 이용해서 회원정보를 저장한다.
 		usersService.insert(dto);
-		ModelAndView mView = new ModelAndView();
-		mView.addObject("msg", dto.getId()+"회원님 가입되었습니다.");
+		ModelAndView mView=new ModelAndView();
+		mView.addObject("msg", dto.getId()+" 회원님 가입되었습니다.");
 		mView.addObject("redirectUri", request.getContextPath());
 		mView.setViewName("users/alert");
 		return mView;
 	}
+	
+	// ajax "/users/checkid.do" 요청 처리
+	@RequestMapping("/users/checkid")
+	@ResponseBody
+	public Map<String, Object> checkid(@RequestParam String inputId){
+		Map<String, Object> map=usersService.canUseId(inputId);
+		// json 문자열 응답하기 
+		return map;
+	}
+	
+	@RequestMapping("/users/signup_form")
+	public String signupForm(){
+		
+		return "users/signup_form";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Spring09_Git/src/main/java/com/gura/spring/users/dao/UsersDaoImpl.java
+++ Spring09_Git/src/main/java/com/gura/spring/users/dao/UsersDaoImpl.java
@@ -19,8 +19,12 @@ public class UsersDaoImpl implements UsersDao{
 
 	@Override
 	public boolean isValid(UsersDto dto) {
-		// TODO Auto-generated method stub
-		return false;
+		UsersDto resultDto = session.selectOne("users.isValid", dto);
+		if(resultDto==null){
+			return false;
+		}else{
+			return true;
+		}
 	}
 
 	@Override
