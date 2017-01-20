4daa34e36fd829bb1c91ce02c85afd1372dd8530
kimjinkuk <kimjinkuk7@naver.com>
2017-01-20 11:24:37 +0900

    회원가입 기능, alert 페이지 추가

--- Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
+++ Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
@@ -2,13 +2,17 @@ package com.gura.spring.users.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gura.spring.users.dto.UsersDto;
 import com.gura.spring.users.service.UsersService;
 
 @Controller
@@ -27,5 +31,15 @@ public class UsersController {
 	public Map<String, Object> checkid(@RequestParam String inputId){
 		Map<String, Object> map = usersService.canUseId(inputId);
 		return map;
+	}
+	
+	@RequestMapping("/users/signup")
+	public ModelAndView signup(HttpServletRequest request ,@ModelAttribute UsersDto dto){
+		usersService.insert(dto);
+		ModelAndView mView = new ModelAndView();
+		mView.addObject("msg", dto.getId()+"회원님 가입되었습니다.");
+		mView.addObject("redirectUri", request.getContextPath());
+		mView.setViewName("users/alert");
+		return mView;
 	}
 }
